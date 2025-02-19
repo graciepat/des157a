d5c97eaf0f5aec9da6d320f56298c7a4df0b5294
Gracie Paterson <gepaterson@ucdavis.edu>
2025-02-19 12:09:47 -0800

    adding experiment for every picture

---
+++ everypicture/experiments/script.js
@@ -0,0 +1,26 @@
+(function(){
+    "use strict";
+    console.log("reading JS");
+
+    const image = document.querySelector('img');
+
+    image.addEventListener('click', function(event){
+        event.preventDefault();
+
+        document.querySelector('#overlay').className = 'showing';
+
+            document.querySelector('.close').addEventListener('click', function(event){
+                event.preventDefault();
+                document.querySelector('#overlay').className = 'hidden';
+            });
+        
+            document.addEventListener('keydown', function(event){
+                if(event.key === 'Escape') {
+                    document.querySelector('#overlay').className = 'hidden';
+                }
+            });
+
+
+    });
+
+})();
